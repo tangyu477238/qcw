922133078680a46b31a601a45908286695786316
tangyu477238 <253060436@qq.com>
2020-04-06 21:07:00 +0800

    初始化

--- src/java/com/bootdo/gc/domain/FukuanDo.java
+++ src/java/com/bootdo/gc/domain/FukuanDo.java
@@ -21,26 +21,32 @@ public class FukuanDo implements Serializable {
     @Excel(name = "到站", orderNum = "4" )
     private String station;
 
-    @Excel(name = "余额", orderNum = "5" ,type=10)
-    private String acbalance;
+
+    @Excel(name = "吨位", orderNum = "5" ,type=10)
+    private String settletonnage;
 
 
+    @Excel(name = "单价", orderNum = "6" ,type=10)
+    private String transcost;
+
+    //运费
+    @Excel(name = "金额", orderNum = "6" ,type=10)
+    private String transfee;
 
 
-
-    @Excel(name = "次数", orderNum = "6" )
+    @Excel(name = "次数", orderNum = "7" )
     private String cishu;
 
-    @Excel(name = "付款金额", orderNum = "7" ,type=10)
+    @Excel(name = "付款金额", orderNum = "8" ,type=10)
     private String fulltrans;
 
-    @Excel(name = "付款方式", orderNum = "8" )
+    @Excel(name = "付款方式", orderNum = "9" )
     private String payType;
 
-    @Excel(name = "收款人", orderNum = "9" )
+    @Excel(name = "收款人", orderNum = "10" )
     private String carrier;
 
-    @Excel(name = "收款账号", orderNum = "10" )
+    @Excel(name = "收款账号", orderNum = "11" )
     private String riskcues;
 
 }
